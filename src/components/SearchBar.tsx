80c7636acc64a44e8af27e4d8d0563b07a589cee
ramrami-B <21801034@handong.ac.kr>
2023-09-07 02:12:25 +0900

    Rename: 컴포넌트 명 변경, 더 쪼개기

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,57 +1,23 @@
-import { styled } from "styled-components";
-import { colors } from "../constants/colors";
-import { IoSearchCircle } from "react-icons/io5";
+import { axiosClient } from "../api/axios";
+import { useState } from "react";
+import RelatedSearch from "./RelatedSearch";
+import Input from "./Input";
 
 const SearchBar = () => {
-  const handlerClickSearch = () => {
-    console.log("검색하기!");
-  };
+  const [isOpen, setIsOpen] = useState(false);
 
   //TODO: 검색 입력 할 때 api 호출
+  const handlerChangeSearchTerms = (e: any) => {
+    console.log(e.target.value);
+    axiosClient.getTerms(e.target.value);
+  };
+
   return (
-    <SearchBarWrap className="search-bar">
-      <SearchBarInput placeholder="질환명을 입력해 주세요"></SearchBarInput>
-      <IoSearchCircle
-        color={colors.primary}
-        size={80}
-        onClick={handlerClickSearch}
-      />
-    </SearchBarWrap>
+    <div>
+      <Input />
+      {isOpen && <RelatedSearch />}
+    </div>
   );
 };
 
 export default SearchBar;
-
-const SearchBarWrap = styled.label`
-  width: 100%;
-  display: flex;
-  border-radius: 3.125rem;
-  background: ${colors.white};
-  padding: 0 1.2rem;
-  margin: 0.6rem 0;
-
-  &:focus-within {
-    border: 2px solid ${colors.primary};
-  }
-`;
-
-const SearchBarInput = styled.input`
-  width: 100%;
-  border-radius: 3.125rem;
-  padding: 1.5rem 1.2rem 1.3rem 1.2rem;
-  border: none;
-  font-size: 1.5rem;
-
-  &:focus {
-    outline: none;
-    background-image: none;
-  }
-
-  &::placeholder {
-    background-image: url(https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-256.png);
-    background-size: 5%;
-    background-position: 0 center;
-    background-repeat: no-repeat;
-    text-indent: 5%;
-  }
-`;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,24 +1,22 @@
 import { styled } from "styled-components";
 import SearchBar from "../components/SearchBar";
-import RelatedSearch from "../components/RelatedSearch";
 
 const SearchPage = () => {
   return (
-    <SearchPageContainer>
+    <Container>
       <h1 style={{ width: "100%" }}>
         국내 모든 임상시험 검색하고
         <br />
         온라인으로 참여하기
       </h1>
       <SearchBar />
-      <RelatedSearch />
-    </SearchPageContainer>
+    </Container>
   );
 };
 
 export default SearchPage;
 
-const SearchPageContainer = styled.div`
+const Container = styled.div`
   width: 50%;
   height: 100%;
   margin: auto;
